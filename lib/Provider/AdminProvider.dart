52195eef8a52d1d367e004341e4e7268da78019c
kishan rathore <rkishan516@gmail.com>
2020-09-04 10:42:34 +0530

    AdminProvider Added

--- lib/Provider/AdminProvider.dart
+++ lib/Provider/AdminProvider.dart
@@ -1,5 +1,25 @@
+import 'package:coach_app/Authentication/FirebaseAuth.dart';
+import 'package:coach_app/Models/model.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/foundation.dart';
 
 class AdminProvider extends ChangeNotifier {
-  
+  AdminProvider() {
+    FirebaseDatabase.instance
+        .reference()
+        .child('institute/${FireBaseAuth.instance.instituteid}/branches')
+        .onValue
+        .listen((event) {
+      event.snapshot.value?.forEach((k, v) {
+        setBranch(k, Branch.fromJson(v));
+      });
+    });
+  }
+
+  Map<String, Branch> branch;
+
+  setBranch(String key, Branch branch) {
+    this.branch[key] = branch;
+    notifyListeners();
+  }
 }
