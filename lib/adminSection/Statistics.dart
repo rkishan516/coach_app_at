fc28ddb19751831af2221154ad7b1b14635564e9
kishan rathore <rkishan516@gmail.com>
2020-09-09 08:43:42 +0530

    Statistics Page added

---
+++ lib/adminSection/Statistics.dart
@@ -0,0 +1,36 @@
+import 'package:coach_app/Models/model.dart';
+import 'package:flutter/material.dart';
+
+class StatisticsPage extends StatefulWidget {
+  final List<Branch> branches;
+  StatisticsPage({@required this.branches});
+  @override
+  _StatisticsPageState createState() => _StatisticsPageState();
+}
+
+class _StatisticsPageState extends State<StatisticsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Admin Statistics',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+                color: Colors.grey.shade200,
+                offset: Offset(2, 4),
+                blurRadius: 5,
+                spreadRadius: 2)
+          ],
+        ),
+      ),
+    );
+  }
+}
