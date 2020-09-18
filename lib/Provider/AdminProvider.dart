b1a1444ea3dba3f716dc15b48125ff62c3fe6815
kishan rathore <rkishan516@gmail.com>
2020-09-18 09:43:10 +0530

    Admin Provider changed for midAdmins

--- lib/Provider/AdminProvider.dart
+++ lib/Provider/AdminProvider.dart
@@ -15,12 +15,28 @@ class AdminProvider extends ChangeNotifier {
         setBranch(k, Branch.fromJson(v));
       });
     });
+    FirebaseDatabase.instance
+        .reference()
+        .child('institute/${FireBaseAuth.instance.instituteid}/midAdmin')
+        .onValue
+        .listen((event) {
+      midAdmins = Map<String, MidAdmin>();
+      event.snapshot.value?.forEach((k, v) {
+        setMidAdmin(k, MidAdmin.fromJson(v));
+      });
+    });
   }
 
   Map<String, Branch> branch;
+  Map<String, MidAdmin> midAdmins;
 
   setBranch(String key, Branch branch) {
     this.branch[key] = branch;
+    notifyListeners();
+  }
+
+  setMidAdmin(String key, MidAdmin midAdmin) {
+    this.midAdmins[key] = midAdmin;
     notifyListeners();
   }
 }
