0fce53ee65a11f91b50b08eb998a50edb1ae3920
kishan rathore <rkishan516@gmail.com>
2020-12-16 20:07:03 +0530

    Functions removed for now

--- functions/node/index.dart
+++ functions/node/index.dart
@@ -1,11 +1,11 @@
-import 'package:firebase_functions_interop/firebase_functions_interop.dart';
+// import 'package:firebase_functions_interop/firebase_functions_interop.dart';
 
-void main() {
-  functions['moin'] = functions.https.onRequest(greet);
-}
+// void main() {
+//   functions['moin'] = functions.https.onRequest(greet);
+// }
 
-void greet(ExpressHttpRequest request) {
-  request.response
-    ..writeln('Moin, moin!')
-    ..close();
-}
+// void greet(ExpressHttpRequest request) {
+//   request.response
+//     ..writeln('Moin, moin!')
+//     ..close();
+// }
